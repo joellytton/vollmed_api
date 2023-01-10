ec449161bc31bb1d9dbb73b471e905daa9c89ab9
joellytton <joellytton@pop-os.localdomain>
2023-01-10 15:03:41 -0500

    melhorias no retorno dos metodos sugerida pelo sonarlint

--- src/main/java/med/voll/api/infra/exception/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/exception/TratadorDeErros.java
@@ -1,6 +1,9 @@
 package med.voll.api.infra.exception;
 
 import jakarta.persistence.EntityNotFoundException;
+
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -9,14 +12,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class TratadorDeErros {
-
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<String> tratarErro404() {
         return  ResponseEntity.notFound().build();
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity tratarErro400(MethodArgumentNotValidException exception) {
+    public ResponseEntity<List<DadosErrosValidacao>> tratarErro400(MethodArgumentNotValidException exception) {
         var erros = exception.getFieldErrors();
         return ResponseEntity.badRequest().body(erros.stream().map(DadosErrosValidacao::new).toList());
     }
